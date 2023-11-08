c0379229ad91973b45d75b7e64ad8fc52d5981a4
Padma Patil <padma@email.com>
2023-11-08 13:39:22 -0800

    Add js

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+function setMidtolid() {
+    const program = document.querySelector('input[name="animal"]:checked').value;
+
+    document.getElementById("snake").setAttribute("class", "hidden");
+    document.getElementById("turtle").setAttribute("class", "hidden");
+    document.getElementById("insect").setAttribute("class", "hidden");
+    document.getElementById("error").setAttribute("class", "hidden");
+
+
+    if (program === "apps") {
+        document.getElementById("snake").removeAttribute("class");
+    } else if (program === "data") {
+        document.getElementById("turtle").removeAttribute("class");
+    } else if (program === "game") {
+        document.getElementById("insect").removeAttribute("class");
+    } else {
+        document.getElementById("error").removeAttribute("class");
+    }
+
+}
+
+function getMidtoLid() {
+    const form = document.querySelector("form");
+    form.onsubmit = function (event) {
+        event.preventDefault();
+        setMidtolid();
+    }
+}
+
+window.onload = function () {
+    getMidtoLid();
+}
